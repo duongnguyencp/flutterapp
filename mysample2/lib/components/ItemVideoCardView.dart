a7ee34fe5cc9b0388a18872a2cdfa7418c2dee3a
duong1 <duongcopeozz@gmail.com>
2023-06-04 09:10:23 +0700

    commit2

--- mysample2/lib/components/ItemVideoCardView.dart
+++ mysample2/lib/components/ItemVideoCardView.dart
@@ -4,12 +4,17 @@ import '../resource/VideoModel.dart';
 import 'CustomListTile.dart';
 import 'DetailModelBottomSheet.dart';
 
-class ItemVideoCardView extends StatelessWidget {
-  final VideoModel viewModel;
+class ItemVideoCardView extends StatefulWidget {
+  final VideoModel viewModel1;
 
-  const ItemVideoCardView({Key? key, required this.viewModel})
+  const ItemVideoCardView({Key? key, required this.viewModel1})
       : super(key: key);
 
+  @override
+  State<ItemVideoCardView> createState() => _ItemVideoCardViewState();
+}
+
+class _ItemVideoCardViewState extends State<ItemVideoCardView> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,19 +22,19 @@ class ItemVideoCardView extends StatelessWidget {
         Stack(
           alignment: AlignmentDirectional.bottomEnd,
           children: [
-            Image(image:AssetImage(viewModel.srcVideoImage)),
+            Image(image: AssetImage(widget.viewModel.srcVideoImage)),
             Container(
                 margin: const EdgeInsets.only(right: 5),
                 padding: const EdgeInsets.all(5),
                 color: Colors.black.withOpacity(0.9),
-                child: Text(viewModel.timeVideo,
+                child: Text(widget.viewModel.timeVideo,
                     style: const TextStyle(
                         fontSize: 14,
                         color: Colors.white,
                         fontFamily: "Roboto,Arial,sans-serif")))
           ],
         ),
-        CustomListTile(videoModel:viewModel)
+        CustomListTile(videoModel: widget.viewModel)
       ],
     );
   }
