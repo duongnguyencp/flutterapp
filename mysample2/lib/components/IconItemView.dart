97d887920e0283745eff7e7c87dbff4860a68dfb
duong1 <duongcopeozz@gmail.com>
2023-06-04 08:56:42 +0700

    Revert "commit convert to stateful"
    This reverts commit 8323eacf49e9094a3431cd0f0963b6ff6ed612b4.

--- mysample2/lib/components/IconItemView.dart
+++ mysample2/lib/components/IconItemView.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Hello extends ConsumerStatefulWidget {
-  const Hello({Key? key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HelloState();
-}
-
-class _HelloState extends ConsumerState<Hello> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class IconItemView extends StatefulWidget {
+class IconItemView extends StatelessWidget {
   final bool selected;
   final String text;
   final IconData icon;
@@ -32,11 +15,6 @@ class IconItemView extends StatefulWidget {
       : super(key: key);
 
   @override
-  State<IconItemView> createState() => _IconItemViewState();
-}
-
-class _IconItemViewState extends State<IconItemView> {
-  @override
   Widget build(BuildContext context) {
     return Wrap(
       spacing: 0,
@@ -44,12 +22,10 @@ class _IconItemViewState extends State<IconItemView> {
       crossAxisAlignment: WrapCrossAlignment.center,
       alignment: WrapAlignment.center,
       children: [
-        IconButton(
-            icon: Icon(widget.icon),
-            color: widget.selected ? Colors.blue : Colors.white,
-            onPressed: widget.onPressed),
-        Text(widget.text,
+        IconButton(icon:Icon(icon),color:selected?Colors.blue:Colors.white, onPressed:onPressed),
+        Text(text,
             style: const TextStyle(
+
                 fontSize: 11,
                 height: .1,
                 fontWeight: FontWeight.bold,
